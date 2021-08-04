ffee807032a19fdc388bee89595cbbc59fa76107
Edward <edwardhorsey@protonmail.com>
2021-08-04 17:47:02 +0100

    Error Message component

---
+++ src/stories/ErrorMessage.tsx
@@ -0,0 +1,14 @@
+export interface ErrorMessageProps {
+  message: string;
+}
+
+export const ErrorMessage = ({
+  message,
+}: ErrorMessageProps): JSX.Element => (
+  <div
+    className="border border-red-400 shadow rounded-md p-4
+    max-w-sm w-full mx-auto text-center"
+  >
+    <p>{message}</p>
+  </div>
+);

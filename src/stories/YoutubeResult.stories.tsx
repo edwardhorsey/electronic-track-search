b9fba140b270bd725e6636e1c0267143a5afa47b
Edward <edwardhorsey@protonmail.com>
2021-08-24 23:28:59 +0100

    youtube

--- src/stories/YoutubeResult.stories.tsx
+++ src/stories/YoutubeResult.stories.tsx
@@ -1,4 +1,6 @@
 import { Meta, Story } from '@storybook/react';
+import { rest } from 'msw';
+import { mockData } from '../mocks/data';
 
 import { YoutubeResult, YoutubeResultProps } from './YoutubeResult';
 
@@ -11,7 +13,38 @@ const Template: Story<YoutubeResultProps> = (
   args: YoutubeResultProps,
 ) => <YoutubeResult {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  embedId: 'XRotY7PuWMc',
+export const Success = Template.bind({});
+Success.args = { artist: 'Oscar Mulero', track: 'Generator' };
+Success.parameters = {
+  msw: [
+    rest.get('/api/youtubeSearch/', (req, res, ctx) => (
+      res(ctx.json({ youtubeResult: mockData.youtubeResult }))
+    )),
+  ],
+};
+
+export const Loading = Template.bind({});
+Loading.args = { artist: 'Oscar Mulero', track: 'Generator' };
+Loading.parameters = {
+  msw: [
+    rest.get('/api/youtubeSearch/', (req, res, ctx) => (
+      res(
+        ctx.delay('infinite'),
+        // ctx.json({ message: 'Custom error message here' }),
+      )
+    )),
+  ],
+};
+
+export const Failure = Template.bind({});
+Failure.args = { artist: 'Oscar Mulero', track: 'Generator' };
+Failure.parameters = {
+  msw: [
+    rest.get('/api/youtubeSearch/', (req, res, ctx) => (
+      res(
+        ctx.status(500),
+        ctx.json({ message: 'Custom error message here' }),
+      )
+    )),
+  ],
 };

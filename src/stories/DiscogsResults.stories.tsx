e1dda1fb0e3805bfc8c0f9d5d704feb1064afc9f
Edward <edwardhorsey@protonmail.com>
2021-08-24 23:25:52 +0100

    discogs

--- src/stories/DiscogsResults.stories.tsx
+++ src/stories/DiscogsResults.stories.tsx
@@ -1,11 +1,7 @@
 import { Meta, Story } from '@storybook/react';
-import { DiscogsResultsReduced } from '../types/types';
-import filterDiscogsResults from '../utils/filterDiscogsResults';
-
+import { rest } from 'msw';
+import { mockData } from '../mocks/data';
 import { DiscogsResults, DiscogsResultsProps } from './DiscogsResults';
-import { discogsData } from '../mocks/data';
-
-const results: DiscogsResultsReduced = filterDiscogsResults(discogsData);
 
 export default {
   title: 'DiscogsResults',
@@ -16,7 +12,38 @@ const Template: Story<DiscogsResultsProps> = (
   args: DiscogsResultsProps,
 ) => <DiscogsResults {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  results,
+export const Success = Template.bind({});
+Success.args = { artist: 'Oscar Mulero', track: 'Generator' };
+Success.parameters = {
+  msw: [
+    rest.get('/api/discogsSearch/', (req, res, ctx) => (
+      res(ctx.json({ discogsResults: mockData.discogsResults }))
+    )),
+  ],
+};
+
+export const Loading = Template.bind({});
+Loading.args = { artist: 'Oscar Mulero', track: 'Generator' };
+Loading.parameters = {
+  msw: [
+    rest.get('/api/discogsSearch/', (req, res, ctx) => (
+      res(
+        ctx.delay('infinite'),
+        // ctx.json({ message: 'Custom error message here' }),
+      )
+    )),
+  ],
+};
+
+export const Failure = Template.bind({});
+Failure.args = { artist: 'Oscar Mulero', track: 'Generator' };
+Failure.parameters = {
+  msw: [
+    rest.get('/api/discogsSearch/', (req, res, ctx) => (
+      res(
+        ctx.status(500),
+        ctx.json({ message: 'Custom error message here' }),
+      )
+    )),
+  ],
 };

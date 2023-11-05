c2a189ce2af1a2ecabdd0680d3842b2be1a549a6
Edward Horsey <edwardhorsey@protonmail.com>
2023-11-05 21:22:05 +0000

    Add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { DarkModeProvider } from '../hooks/useDarkMode';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <DarkModeProvider>
             <Component {...pageProps} />
+            <Analytics />
         </DarkModeProvider>
     );
 }
